f54d864be6742714e097476292cbf30edd897e0f
nobila <anjum.nobila@gmail.com>
2017-02-13 03:31:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 //Move image on clicking it
 var img=document.getElementById('madi');
-img.onClick=function()
+img.onclick=function()
 {
     
     img.style.marginleft='100px';
